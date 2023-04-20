a4f04c4f3eb28218d88bac96828e8d8981073b14
Zayn Xie <84443886+xiaoniuren99@users.noreply.github.com>
2023-04-20 21:11:23 +0800

    commit

--- auto.py
+++ auto.py
@@ -128,11 +128,11 @@ for key in FETCH_LIST.keys():
     list_articles = fetch_one_module(url)
     msg = build_msg_one_module(list_articles, name)
 
-    # send to wechat
-    try:
-        requests.post(url=CALLBACK_URL, params={"msg": msg})
-    except Exception as e:
-        logging.info("callback error")
+    # # send to wechat
+    # try:
+    #     requests.post(url=CALLBACK_URL, params={"msg": msg})
+    # except Exception as e:
+    #     logging.info("callback error")
 
     # send to wechat room
     try:
